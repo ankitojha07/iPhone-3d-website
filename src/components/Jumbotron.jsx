b1b0384b7f3294c1d638470f176d4999ec69c619
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-25 02:03:13 +0530

    working on section 2

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import IPhone from "../assets/images/iphone-14.jpg";
+import HoldingIphone from "../assets/images/iphone-hand.png";
 const Jumbotron = () => {
   return (
     <div className="jumbotron-section wrapper">
@@ -9,6 +10,18 @@ const Jumbotron = () => {
       <span className="description">
         From $41.62/mo. for 24 mo. or $999 before trade-in
       </span>
+      <ul className="links">
+        <li>
+          <button className="button">Buy</button>
+        </li>
+
+        <li>
+          <a href="" className="link">
+            Learn more
+          </a>
+        </li>
+      </ul>
+      <img src={HoldingIphone} alt="iPhone" className="iphone-img" />
     </div>
   );
 };
